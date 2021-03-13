3deeabbd22eba9dd28a2e0ca9ae364ba325223b7
AngelGris76 <sebastian.smuraglia@gmail.com>
2021-03-13 16:19:54 -0300

    Correccion semantica, estilos, y funciones JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,33 @@
-const faq = document.getElementById('faq');
+const faq = document.getElementById('faq__list');
+const box = document.getElementById('box');
 
 faq.addEventListener('click', (e) =>
 {
-    let faqQuestion;
-        
-    if (e.target.classList.contains('faq__img')) faqQuestion = e.target.parentElement;
-    else
-        faqQuestion = e.target;
+    let elementTarget;
 
-    if (faqQuestion.classList.contains('faq__question'))
+    if (e.target.classList.contains('faq__question')) elementTarget = e.target;
+    else
+        if (e.target.classList.contains('faq__question-arrow')) elementTarget = e.target.previousElementSibling;
+
+    elementTarget.classList.toggle('faq__question--bold')
+    elementTarget.nextElementSibling.classList.toggle('faq__question-arrow--active');
+    elementTarget.parentElement.nextElementSibling.classList.toggle('faq__answer--hide');
+});
+
+faq.addEventListener('mouseover', (e) =>
+{
+    if (e.target.classList.contains('faq__question') )
     {
-        faqQuestion.classList.toggle('faq__question--open');
-        faqQuestion.children[0].classList.toggle('faq__img--open');
-        faqQuestion.nextElementSibling.classList.toggle('faq__answer--open');
+        box.classList.add('box--hover');
+        e.target.classList.toggle('faq__question--hover');
+    }
+});
+
+faq.addEventListener('mouseout', (e) =>
+{
+    if (e.target.classList.contains('faq__question'))
+    {
+        box.classList.remove('box--hover');
+        e.target.classList.toggle('faq__question--hover');
     }
 });
